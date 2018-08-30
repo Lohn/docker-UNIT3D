ef10c6c37cd1a69cba624e2cd0a5b70d751a1ea3
Toothwitch <witch@toothwit.ch>
2018-08-30 11:01:30 +0200

    Fixed the startup.sh Still need to kill the container somehow when the application dies
    Added an initialize parameter for database initialization

--- startup.sh
+++ startup.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+cp .env.example .env
+sed -i "s|DB_HOST=127.0.0.1|DB_HOST=$DB_HOST |g" .env
+sed -i "s|DB_DATABASE=unit3d|DB_DATABASE=$DB_DATABASE |g" .env
+sed -i "s|DB_USERNAME=homestead|DB_USERNAME=$DB_USERNAME |g" .env
+sed -i "s|DB_PASSWORD=secret|DB_PASSWORD=$DB_PASSWORD |g" .env
+sed -i "s|APP_URL=http://unit3d.site|APP_URL=$APP_URL |g" .env
+if [ $INITIALIZE -eq 1]
+then
 /usr/bin/php artisan migrate --seed
+fi
 /etc/init.d/supervisor start
 supervisorctl reread && supervisorctl update
+/bin/bash
